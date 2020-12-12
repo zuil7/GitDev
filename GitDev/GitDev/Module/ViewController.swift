50e4b4ef90b01e451668339d1462ba5805e7130d
louisenamoc <louise.namoc@appetiser.com.au>
2020-12-13 07:51:01 +0800

    'Initial commit'

---
+++ GitDev/GitDev/Module/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  GitDev
+//
+//  Created by Louise Namoc.
+//  Copyright © 2020 Louise Namoc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+}
